aa82588e66d8c222fd97640c927c62a62ec29659
KeshavGoel2011 <eagleplays2011@gmail.com>
2025-02-01 18:52:01 +0530

    Lesson 6

---
+++ Lesson 6/And or operators.py
@@ -0,0 +1,20 @@
+a = 4
+b = 10
+c = 8
+
+if a and b and c:
+    print("All the numbers have boolean value as true")
+else:
+    print("Atleast one number has boolean value as False")
+
+a = 7
+b = -6
+c = 0
+if a>0 or b>0:
+    print("Either of the number is greater than zero")
+else:
+    print("No number is greater than 0")
+if b>0 or c>0:
+       print("Either of the number is greater than zero")
+else:
+    print("No number is greater than 0")
---
+++ Lesson 6/not equal operator.py
@@ -0,0 +1,20 @@
+a = 8
+b = 6
+c = 6
+
+print(a != b)
+print (b != c)
+
+a = "python"
+b = "codingal"
+if a !=  b:
+    print(a, "and", b, "are different")
+
+a = 4
+b = 5
+if (a == 1) != (b == 5):
+    print ("Hello")
+
+a = int(input("Enter a number: "))
+if a%2 != 0:
+    print ("The number is odd")
