d84f86c48bdd6a1b53e4b7d92e21d9d68136f936
KeshavGoel2011 <eagleplays2011@gmail.com>
2025-03-23 23:11:15 +0530

    Lesson 13

---
+++ Lesson 13/Right angled triangle.py
@@ -0,0 +1,9 @@
+#right angled triangle of stars
+print("Right angled triangle of stars pattern")
+n = int(input("Enter the number of rows: "))
+
+for i in range (n):
+    for j in range (i+1):
+     print("1",end = "")
+
+    print ()
